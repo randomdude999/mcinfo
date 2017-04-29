cdf6b1a2bb99ebbec40ea3a771fc1b594b3548da
randomdude999 <just.so.you.can.email.me@gmail.com>
2017-04-29 20:19:48 +0300

    maybe get the recipes working too
    no test data written yet so don't know!

--- mcinfo/normal_info.py
+++ mcinfo/normal_info.py
@@ -1,6 +1,7 @@
 import pkg_resources
 import json
 import textwrap
+import mcinfo
 
 
 def format_data(data):
@@ -12,10 +13,18 @@ def format_data(data):
 
 
 def handle_request(request):
-    # TODO also show recipes
+    out = ""
     if pkg_resources.resource_exists(__name__, "data/%s.json" %
                                      request):
         stream = pkg_resources.resource_stream(__name__, "data/%s.json" %
                                                request)
         data = json.load(stream)
-        return format_data(data)
+        out = format_data(data)
+
+    if pkg_resources.resource_exists(__name__, "data/recipes/%s.json" %
+                                     request):
+        stream = pkg_resources.resource_stream(__name__, "data/recipes/%s.json"
+                                               % request)
+        data = json.load(stream)
+        out += str(mcinfo.recipes.RecipeCollection(data))
+    return out
